8e9afe7e772d2926d23f5102ac61c98175e7779b
marinasavchenko <msavchenko.abacuspro@gmail.com>
2018-06-01 17:01:15 +0200

    add Spring SecurityConfiguration

---
+++ javatutor/src/main/java/pro/abacus/javatutor/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package pro.abacus.javatutor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+
+}
