db3a4a10cb5f66fa26d46601b1f5572862a9329f
marinasavchenko <msavchenko.abacuspro@gmail.com>
2018-06-25 10:25:58 +0200

    add comments to repositories

--- javatutor/src/main/java/pro/abacus/javatutor/repository/AuthorityRepository.java
+++ javatutor/src/main/java/pro/abacus/javatutor/repository/AuthorityRepository.java
@@ -1,13 +1,14 @@
 package pro.abacus.javatutor.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import pro.abacus.javatutor.domain.Authority;
 
 /**
  * Spring Data MongoDB repository for the Authority entity.
  */
-
+@Repository
 public interface AuthorityRepository extends MongoRepository<Authority, String> {
 
 }
--- javatutor/src/main/java/pro/abacus/javatutor/repository/UserRepository.java
+++ javatutor/src/main/java/pro/abacus/javatutor/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package pro.abacus.javatutor.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import pro.abacus.javatutor.domain.User;
 
+/**
+ * Spring Data MongoDB repository for the User entity.
+ */
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 	
 	public User findByUsername(String username);
