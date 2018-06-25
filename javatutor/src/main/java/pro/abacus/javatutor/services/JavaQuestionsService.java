a72865e395eab41bd8c55d01587588ec1e27b70b
marinasavchenko <msavchenko.abacuspro@gmail.com>
2018-06-25 16:20:14 +0200

    add comments

--- javatutor/src/main/java/pro/abacus/javatutor/services/JavaQuestionsService.java
+++ javatutor/src/main/java/pro/abacus/javatutor/services/JavaQuestionsService.java
@@ -4,6 +4,9 @@ import java.util.Collection;
 
 import pro.abacus.javatutor.domain.JavaQuestion;
 
+/**
+ * Service interface for managing Java Questions.
+ */
 public interface JavaQuestionsService {
 
 	public Collection<JavaQuestion> findAllQuestions();
--- javatutor/src/main/java/pro/abacus/javatutor/services/UserService.java
+++ javatutor/src/main/java/pro/abacus/javatutor/services/UserService.java
@@ -2,6 +2,10 @@ package pro.abacus.javatutor.services;
 
 import pro.abacus.javatutor.domain.User;
 
+
+/**
+ * Service interface for managing users.
+ */
 public interface UserService {
 	
 	public User saveAccount(User user);
