9ae350880446960735227ddff694cfe5e166d606
marinasavchenko <msavchenko.abacuspro@gmail.com>
2018-10-11 10:52:36 +0200

    deleted unused configuration from JavaTutorApplication

--- javatutor/src/main/java/pro/abacus/javatutor/JavaTutorApplication.java
+++ javatutor/src/main/java/pro/abacus/javatutor/JavaTutorApplication.java
@@ -6,13 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JavaTutorApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(JavaTutorApplication.class);
-	}
-
+public class JavaTutorApplication {
 	/**
 	 * Main method, used to run the application.
 	 * 
