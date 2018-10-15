f13cc46d3c9061fbbcaf916bf684218eecaf394c
marinasavchenko <msavchenko.abacuspro@gmail.com>
2018-10-15 09:30:49 +0200

    deleted redundand access modifiers

--- javatutor/src/main/java/pro/abacus/javatutor/service/JavaQuestionsService.java
+++ javatutor/src/main/java/pro/abacus/javatutor/service/JavaQuestionsService.java
@@ -1,14 +1,13 @@
 package pro.abacus.javatutor.service;
 
-import java.util.Collection;
-
 import pro.abacus.javatutor.domain.JavaQuestion;
+
+import java.util.Collection;
 
 /**
  * Service interface for managing Java Questions.
  */
 public interface JavaQuestionsService {
-
-	public Collection<JavaQuestion> findAllQuestions();
-	public void saveAllQuestions(Collection<JavaQuestion> javaQuestions);
+	Collection<JavaQuestion> findAllQuestions();
+	void saveAllQuestions(Collection<JavaQuestion> javaQuestions);
 }
--- javatutor/src/main/java/pro/abacus/javatutor/service/UserService.java
+++ javatutor/src/main/java/pro/abacus/javatutor/service/UserService.java
@@ -7,7 +7,6 @@ import pro.abacus.javatutor.domain.User;
  * Service interface for managing users.
  */
 public interface UserService {
-	
-	public User saveAccount(User user);
+	User saveAccount(User user);
 
 }
