f30591c3c809f9644fc5af6b82bca12560fc077c
marinasavchenko <msavchenko.abacuspro@gmail.com>
2018-10-16 12:04:35 +0200

    split SecurityIntegtationTest into 2 that talks to databace

---
+++ javatutor/src/test/java/pro/abacus/javatutor/SecurityIntegrationDatabaseTest.java
@@ -0,0 +1,61 @@
+package pro.abacus.javatutor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SecurityIntegrationDatabaseTest {
+	private static final String SIGN_IN  = "/api/signin";
+	private static final String JAVA_QUESTIONS  = "/api/javaquestions/";
+
+	private static final String VALID_LOGIN  = "Tom";
+	private static final String VALID_PASSWORD  = "qwerty";
+
+	private static final String INVALID_LOGIN  = "invalidLogin";
+	private static final String INVALID_PASSWORD  = "invalidPassword";
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void shouldAuthenticateUserWithValidCredentials() throws Exception {
+		this.mockMvc.perform(formLogin(SIGN_IN).user(VALID_LOGIN).password(VALID_PASSWORD))
+				.andExpect(authenticated());
+	}
+
+	@Test
+	public void shouldAccessProtectedEndpoinAfterSuccessfulLogin() throws Exception {
+		MvcResult mvcResult = this.mockMvc.perform(formLogin(SIGN_IN).user(VALID_LOGIN).password(VALID_PASSWORD))
+				.andExpect(authenticated())
+				.andReturn();
+
+		MockHttpSession httpSession = MockHttpSession.class.cast(mvcResult.getRequest().getSession(false));
+
+		this.mockMvc.perform(get(JAVA_QUESTIONS)
+				.session(httpSession))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void shouldNotAuthenticateUserWithInvalidCredentials() throws Exception {
+		this.mockMvc.perform(formLogin(SIGN_IN).user(INVALID_LOGIN).password(INVALID_PASSWORD))
+				.andExpect(unauthenticated())
+				.andExpect(status().is4xxClientError());
+	}
+
+}
--- javatutor/src/test/java/pro/abacus/javatutor/SecurityIntegrationTest.java
+++ javatutor/src/test/java/pro/abacus/javatutor/SecurityIntegrationTest.java
@@ -5,15 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
-import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -23,14 +19,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class SecurityIntegrationTest {
 
 	private static final String JAVA_QUESTIONS  = "/api/javaquestions/";
-	private static final String SIGN_IN  = "/api/signin";
-
-	private static final String VALID_LOGIN  = "Tom";
-	private static final String VALID_PASSWORD  = "qwerty";
-
-	private static final String INVALID_LOGIN  = "invalidLogin";
-	private static final String INVALID_PASSWORD  = "invalidPassword";
-
 	
 	@Autowired
 	private MockMvc mockMvc;
@@ -42,38 +30,10 @@ public class SecurityIntegrationTest {
 	}
 	
 	@Test
-	public void shouldAuthenticateUserWithValidCredentials() throws Exception {
-		this.mockMvc.perform(formLogin(SIGN_IN).user(VALID_LOGIN).password(VALID_PASSWORD))
-				.andExpect(authenticated());
-	}
-		
-	@Test
-	public void shouldNotAuthenticateUserWithInvalidCredentials() throws Exception {
-		this.mockMvc.perform(formLogin(SIGN_IN).user(INVALID_LOGIN).password(INVALID_PASSWORD))
-				.andExpect(unauthenticated())
-				.andExpect(status().is4xxClientError());
-	}
-	
-	
-	@Test
 	@WithMockUser
 	public void shouldAuthenticateMockUser() throws Exception {
 		this.mockMvc.perform(get(JAVA_QUESTIONS))
 				.andExpect(authenticated());
 	}
-	
-	@Test
-	public void sholdAccessProtectedEndpoinAfterSuccessfulLogin() throws Exception {
-		MvcResult mvcResult = this.mockMvc.perform(formLogin(SIGN_IN).user(VALID_LOGIN).password(VALID_PASSWORD))
-				.andExpect(authenticated())
-				.andReturn();
-		
-		MockHttpSession httpSession = MockHttpSession.class.cast(mvcResult.getRequest().getSession(false));
-
-		this.mockMvc.perform(get(JAVA_QUESTIONS)
-				.session(httpSession))
-				.andExpect(status().isOk());
-	}
-
 
 }
