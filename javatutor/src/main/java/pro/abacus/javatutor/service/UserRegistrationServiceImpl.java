e5d9f7f1744820a502fb987a5f30c106176b6de4
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-02-05 20:09:20 +0100

    changed BCryptPasswordEncoder to PasswordEncoder

--- javatutor/src/main/java/pro/abacus/javatutor/service/UserRegistrationServiceImpl.java
+++ javatutor/src/main/java/pro/abacus/javatutor/service/UserRegistrationServiceImpl.java
@@ -1,7 +1,7 @@
 package pro.abacus.javatutor.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pro.abacus.javatutor.domain.User;
 import pro.abacus.javatutor.repository.AuthorityRepository;
@@ -13,29 +13,30 @@ import java.util.HashSet;
 public class UserRegistrationServiceImpl implements UserRegistrationService {
 
 	private UserRepository userRepository;
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	private PasswordEncoder passwordEncoder;
 	private AuthorityRepository authorityRepository;
 
 	@Autowired
-	public UserRegistrationServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder,
+	public UserRegistrationServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder,
 	                                   AuthorityRepository authorityRepository) {
 		this.userRepository = userRepository;
-		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+		this.passwordEncoder = passwordEncoder;
 		this.authorityRepository = authorityRepository;
 	}
 
 	@Override
 	public void saveUserAccount(User user) {
 		setEncodedPassword(user);
-		setAuthoritisForAccount(user);
+		setAuthoritiesForAccount(user);
 		userRepository.save(user);
 	}
 
 	private void setEncodedPassword(User user) {
-		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
 	}
 
-	private void setAuthoritisForAccount(User user) {
+	private void setAuthoritiesForAccount(User user) {
 		user.setUserAuthorities(new HashSet<>(authorityRepository.findAll()));
 	}
+
 }
