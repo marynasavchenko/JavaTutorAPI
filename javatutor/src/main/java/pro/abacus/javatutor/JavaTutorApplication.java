d1b9309f86d56e8d176f5e7cf8d2bc2260ed4698
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-14 08:32:04 +0100

    modified javadoc

--- javatutor/src/main/java/pro/abacus/javatutor/JavaTutorApplication.java
+++ javatutor/src/main/java/pro/abacus/javatutor/JavaTutorApplication.java
@@ -3,12 +3,15 @@ package pro.abacus.javatutor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Boot main class that bootstrap this project.
+ */
 @SpringBootApplication
 public class JavaTutorApplication {
 	/**
-	 * Main method, used to run the application.
+	 * Main method, used to run this application.
 	 *
-	 * @param args the command line arguments
+	 * @param args the string array, that contains command line arguments
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(JavaTutorApplication.class, args);
