e1c451637dc8ffb0508b63efe06206cfce78956d
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-15 12:54:47 +0100

    fixed typos in doc comments

--- javatutor/src/main/java/pro/abacus/javatutor/JavaTutorApplication.java
+++ javatutor/src/main/java/pro/abacus/javatutor/JavaTutorApplication.java
@@ -4,14 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Spring Boot main class that bootstrap this project.
+ * Spring Boot main class that bootstraps this project.
  */
 @SpringBootApplication
 public class JavaTutorApplication {
 	/**
 	 * Main method, used to run this application.
 	 *
-	 * @param args the string array, that contains command line arguments
+	 * @param args the string array, that contains command line arguments.
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(JavaTutorApplication.class, args);
